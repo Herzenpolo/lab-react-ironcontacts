2c4ba1251bc07cc307693eebac953f19073bf1bd
Herzen Cortes <herzencc@icloud.com>
2020-04-21 14:50:39 -0400

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
 
 class App extends Component {
+  
+state = {
+  fiveContacts: contacts.slice(0, 5) // cuts the contact array and leaves indexes 0-5
+}
+
+displayFive = () => {
+  let newArr = this.state.fiveContacts.map(eachContact => {
+    return(
+      <tr key = {eachContact.name}>
+      <td><img width = "50px" src={eachContact.pictureUrl} alt="actor"/></td>
+      <td>{eachContact.name}</td>
+      <td>{eachContact.popularity}</td>
+   </tr>
+    )
+  })
+  return newArr
+}
+
   render() {
+    console.log(this.state.fiveContacts)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <table>
+         <thead>
+           <tr>
+             <th> Picture </th>
+             <th> Name </th>
+             <th> Popularity </th>
+           </tr>
+         </thead>
+         <tbody>
+         {this.displayFive()}
+         </tbody>
+       </table>
       </div>
     );
   }
